c73c24f010ae101e7192b3540be661ed2a90e246
David D. <david.diego.d@gmail.com>
2019-01-26 22:49:09 +0000

    Now I have a default.html file that I can use to see all the scripts. It's still messy, but it's nice. I'm still missing the content of the story.

--- scripts/preview-format.js
+++ scripts/preview-format.js
@@ -9,35 +9,20 @@ var twine = require('twine-utils');
 var encoding = { encoding: 'utf8' };
 
 Promise.all([
-	exec('cssnano src/*.css'),
-	exec('browserify -g uglifyify src/index.js', { maxBuffer: Infinity })
+	exec('browserify src/index.js', { maxBuffer: Infinity })
 ]).then(function(results) {
-	var distPath = 'public';
+	var distPath = 'preview';
 	var htmlTemplate = ejs.compile(fs.readFileSync('src/index.ejs', encoding));
-	var formatData = {
-		author: pkg.author.replace(/ <.*>/, ''),
-		description: pkg.description,
-		image: 'icon.svg',
-		name: pkg.name,
-		proofing: false,
-		source: htmlTemplate({
-			style: results[0].stdout,
-			script: results[1].stdout
-		}),
-		url: pkg.repository,
-		version: pkg.version
-	};
+
 
 	mkdirp.sync(distPath);
 	fs.writeFileSync(
-		distPath + '/format.js',
-		'window.storyFormat(' + JSON.stringify(formatData) + ');'
+		distPath + '/default.html',
+		htmlTemplate({
+			style: fs.readFileSync('src/basic.css'),
+			script: results[0].stdout
+		})
 	);
-	cpx.copySync('src/basic.css', distPath);
-	cpx.copySync('src/icon.svg', distPath);
-	cpx.copySync('src/index.ejs', distPath);
-	cpx.copySync('src/index.js', distPath);
-	cpx.copySync('src/passage.js', distPath);
-	cpx.copySync('src/story.js', distPath);
+
 
 });
